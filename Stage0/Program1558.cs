89f5728bd7cf28198f609ecc4c4f222a72742e49
Pinni Plevinski <shareytora007@gmail.com>
2023-12-26 22:17:56 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program1558.cs
@@ -0,0 +1,19 @@
+﻿namespace Stage0;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Welcome1558();
+        Welcome3645();
+        Console.ReadKey();
+    }
+    static partial void Welcome3645();
+
+    private static void Welcome1558()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine(name + ", welcome to my first console application.");
+    }
+}
